b44d168da86d3dda043a7cbdd08a1b351f4c9194
Ross Hunter <hunter.dev.87@gmail.com>
2019-10-11 02:21:52 -0500

    Update copy in project type cards

--- src/components/ProjectTypeGrid/index.js
+++ src/components/ProjectTypeGrid/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ProjectCard from 'components/ProjectCard';
-import Divider from 'legos/Divider';
+import React from "react";
+import ProjectCard from "components/ProjectCard";
+import Divider from "legos/Divider";
 
-import './styles.scss';
+import "./styles.scss";
 
-const ProjectTypeGrid = props => {
-	return(
-		<React.Fragment>
-			<Divider id='skills'>Skills</Divider>
-			<div className="c-ProjectTypeGrid">
-				<ProjectCard theme='react' title="React">
-					Redux, Jest, Enzyme.
-				</ProjectCard>
+const ProjectTypeGrid = (props) => {
+    return (
+        <React.Fragment>
+            <Divider id="skills">Skills</Divider>
+            <div className="c-ProjectTypeGrid">
+                <ProjectCard theme="react" title="React">
+                    TypeScript, Redux, Jest.
+                </ProjectCard>
 
-				<ProjectCard theme='js' title="Javascript" isInverted={true}>
-					Node, Express, and Mongo, with plenty of vanilla for good measure.
-				</ProjectCard>
+                <ProjectCard theme="js" title="Javascript" isInverted={true}>
+                    Node, Express, and Mongo.
+                </ProjectCard>
 
-				<ProjectCard theme='css' title="HTML/CSS">
-					Responsive, adaptable layouts with grid, flexbox, and BEM.
-				</ProjectCard>
-			</div>
-		</React.Fragment>
-	)
-}
+                <ProjectCard theme="css" title="HTML/CSS">
+                    Responsive, adaptable layouts with grid, SASS, and BEM.
+                </ProjectCard>
+            </div>
+        </React.Fragment>
+    );
+};
 
 export default ProjectTypeGrid;
