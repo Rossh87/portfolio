57830b3f5087b21a965383df76e8c6c203737b30
Ross Hunter <hunter.dev.87@gmail.com>
2020-01-08 22:39:07 -0600

    Change text in about-me section
    'testable' should be 'tested'

--- src/components/HomeAboutSection/index.js
+++ src/components/HomeAboutSection/index.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React from "react";
 
 // Get HOC to handle hover behavior on touch screens
-import withHoverBehaviors from 'hocs/withHoverBehaviors';
+import withHoverBehaviors from "hocs/withHoverBehaviors";
 
-import './styles.scss';
+import "./styles.scss";
 
-const HomeAboutSection = ({isHovered, HOCRef, handleMouseEnter, handleMouseLeave}) => {
-	const buildClassNames = () => {
-		return isHovered ?
-			'c-about s-is-hovered'
-			:
-			'c-about'
-	}
+const HomeAboutSection = ({
+    isHovered,
+    HOCRef,
+    handleMouseEnter,
+    handleMouseLeave
+}) => {
+    const buildClassNames = () => {
+        return isHovered ? "c-about s-is-hovered" : "c-about";
+    };
 
-	return(
-		// ID added for navigation.  Ref is passed by HOC to manage hover
-		// behaviors when mouse not in use.
-		<section 
-			className={buildClassNames()}
-			ref={HOCRef}
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}
-		>
-			<div className="c-about_text">
-				<h1>Welcome! I'm Ross.</h1>
-				<p>I'm a self-taught web developer who's passionate about software that rises to real-world challenges.  I strive to write modular, testable code that's easy to read and easy to change.  Below you'll find some projects I'm excited about right now.
-				</p>
-			</div>
-			
-		</section>
-	)
-}
+    return (
+        // ID added for navigation.  Ref is passed by HOC to manage hover
+        // behaviors when mouse not in use.
+        <section
+            className={buildClassNames()}
+            ref={HOCRef}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <div className="c-about_text">
+                <h1>Welcome! I'm Ross.</h1>
+                <p>
+                    I'm a web developer who's passionate about software that
+                    rises to real-world challenges. I strive to write modular,
+                    tested code that's easy to read and easy to change. Below
+                    you'll find some projects I'm excited about right now.
+                </p>
+            </div>
+        </section>
+    );
+};
 
 export default withHoverBehaviors(HomeAboutSection);
